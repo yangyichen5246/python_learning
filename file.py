6190afec7c8a4e88efe5731f0d51db9f886415f6
yangyichen1 <yangyichen1@xiaozhilc.com>
2018-09-26 19:50:49 +0800

    ceshi

--- file.py
+++ file.py
@@ -1,7 +1,19 @@
 
 
-with open("./ceshi_09_26.txt",'r') as m_file:
+with open("./ceshi_09_26.txt") as m_file:
     m_file.read()
     # print("file open sucess")
     # contents = m_file(read)
     # print(contents)
+
+# filepath = './c/ceshi_09_26.txt'
+# with open(filepath) as file_object:
+#      for line in file_object:
+#         print(line)
+
+
+filename = 'loving.txt'
+
+with open (filename,'w')as file_object:
+    file_object.write("i loving programing.\n")
+    file_object.write("i loving yichen.\n")
