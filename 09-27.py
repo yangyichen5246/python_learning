83f27722b2ec49310cc2a160108f202ee638aac6
yangyichen1 <yangyichen1@xiaozhilc.com>
2018-09-28 11:49:06 +0800

    update

--- 09-27.py
+++ 09-27.py
@@ -1,21 +1,22 @@
 import random
 secretNumber = random.randint(1,20)
-print('i am thinking of a number between 1 and 20.')
+print('来，咋们玩一个游戏，猜一个1-20间的数值.')
 
-for guessesTaken in range(1,7):
-    print('take a guess ,请在下方输入一个数字:')
+for guessesTaken in range(1,8):
+    print('游戏开始,请在下方输入一个数字:')
     guess = int(input())
 
 
 
     if guess < secretNumber:
-        print('your guess is too low')
+        print('你猜的太小了')
     elif guess > secretNumber:
-        print('your guess is too high')
+        print('你猜的太大了')
     else:
         break
 if guess == secretNumber:
-   print("good job your guess is in " + str(guessesTaken)+" guess!")
+   print('bingo，我猜的数值就是' + str(secretNumber))
+   print("非常棒，猜对了 只用了" + str(guessesTaken) + "次")
 else:
-   print('nope the number i was thinking was' + str(secretNumber))
+   print('错了其实我猜的数值是' + str(secretNumber))
 
